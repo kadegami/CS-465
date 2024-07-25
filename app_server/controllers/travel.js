b8b590a42090e21625d1a74aaca937e0f6172101
Karina Washington <kadegami@hotmail.com>
2024-07-24 21:41:49 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync(".data/trips.json", "utf8"));
+
 /* GET Homepage*/
 const index = (req, res) => {
-  res.render("travel", { title: "Travlr Getaways" });
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 
 module.exports = {
