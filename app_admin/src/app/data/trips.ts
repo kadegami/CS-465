08a8e8de39ab441c8e5b2bf272a76fafbda6b111
Karina Washington <kadegami@hotmail.com>
2024-08-05 14:10:52 -0500

    first commit

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,35 @@
+export const trips = [
+  {
+    code: 'GALR210214',
+    name: 'Gale Reef',
+    image: 'reef1.jpg',
+    length: '4 night / 5 days',
+    start: '2021-02-14T08:00:00Z',
+    resort: 'Emerald Bay, 3 stars',
+    perPerson: '799.00',
+    description:
+      '<p> Gale Reef Sef et auge lorem. In sit amet placerat arcu </p>',
+  },
+  {
+    code: 'DAWR210315',
+    name: "Dawson's Reef",
+    length: '4 night / 5 days',
+    start: '2021-03-15T08:00:00Z',
+    resort: 'Blue Lagoon, 4 stars',
+    image: 'reef2.jpg',
+    perPerson: '999.00',
+    description:
+      "<p> Dawnson's Reef integer magna, leo posuere et dignissim v </p>",
+  },
+  {
+    code: 'CLAR210621',
+    name: "Claire's Reef",
+    length: '4 night / 5 days',
+    start: '2021-06-21T08:00:00Z',
+    resort: 'Coral Sands, 5 stars',
+    perPerson: '1999.00',
+    image: 'reef3.jpg',
+    description:
+      "<p> Claire's Reef donec sed feliz risus. Nulla Facilisi. Do </p>",
+  },
+];
